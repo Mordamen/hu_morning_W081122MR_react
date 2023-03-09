fb91b8e4797e2551e50119c411e9ade11c78412c
Mordamen <43504758+Mordamen@users.noreply.github.com>
2023-03-09 14:32:39 +0200

    Adding in Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
 import { Container } from "@mui/material";
 import "./App.css";
-import CardComponent from "./components/CardComponent";
-import FirstComponent from "./components/FirstComponent";
-import GridComponent from "./components/GridComponent";
-import ListComponent from "./components/ListComponent";
+// import CardComponent from "./components/CardComponent";
+// import FirstComponent from "./components/FirstComponent";
+// import GridComponent from "./components/GridComponent";
+// import ListComponent from "./components/ListComponent";
 import MuiNavbar from "./components/Navbar/MuiNavbar";
-import Navbar from "./components/Navbar/Navbar";
-import HomePage from "./pages/HomePage";
-import RegisterPage from "./pages/RegisterPage";
-import Login from "./pages/SignInPage";
+// import Navbar from "./components/Navbar/Navbar";
+// import HomePage from "./pages/HomePage";
+// import RegisterPage from "./pages/RegisterPage";
+// import Login from "./pages/LoginPage";
+import Router from "./routes/Router";
 
 function App() {
   return (
     <Container>
-      <MuiNavbar />
-      {/* <Navbar /> */}
-      {/* <FirstComponent /> */}
-      {/* <ListComponent /> */}
-      {/* <GridComponent /> */}
-      <HomePage />
-      {/* <RegisterPage /> */}
-      {/* <HomePage /> */}
-      {/* <Login /> */}
+      <header>
+        <MuiNavbar />
+      </header>
+      <main>
+        <Router />
+      </main>
+      <footer></footer>
     </Container>
   );
 }
